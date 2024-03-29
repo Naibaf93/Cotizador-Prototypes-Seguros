961e0ff229a4c9ca7436b69f4aa89c9d4ce2787d
Fabián Huertas <miloh9318@hotmail.com>
2024-03-28 21:38:33 -0500

    crear un prototype para mostrar un error

--- js/app.js
+++ js/app.js
@@ -23,10 +23,30 @@ ui.prototype.llenarOpciones = () => {
     }
 }
 
+//Muestra alertas en pantalla
+ui.prototype.mostrarMensaje = (mensaje, tipo) => {
+    const div = document.createElement('div');
+
+    if (tipo === 'error') {
+        div.classList.add('error');
+    } else {
+        div.classList.add('correcto');
+    }
+    div.classList.add('mensaje', 'mt-10');
+    div.textContent = mensaje;
+
+    // Insertar en el HTML
+    const formulario = document.querySelector('#cotizar-seguro');
+    formulario.insertBefore(div, document.querySelector('#resultado'));
+
+    setTimeout(() => {
+        div.remove();
+    }, 3000);
+}
+
 //Instanciar UI
 
 const UI = new ui();
-console.log(UI);
 
 document.addEventListener('DOMContentLoaded', () => {
     UI.llenarOpciones(); // Llena el select con los años
@@ -48,8 +68,13 @@ function cotizarSeguro(e) {
     const tipo = document.querySelector('input[name="tipo"]:checked').value;
 
     if(marca === '' || year === '' || tipo === '') {
-        console.log('No paso la validacion');
-    } else {
-        console.log('Si paso la validacion');
-    }
+        UI.mostrarMensaje('Todos los campos son obligatorios', 'error');
+        return;
+    } 
+    
+    UI.mostrarMensaje('Cotizando...', 'exito');
+
+    // Instanciar el seguro 
+
+    // Utilizar el prototype que va a cotizar
 }
